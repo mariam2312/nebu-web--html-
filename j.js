7b37139e544e280f8453126eee9a840f3a70301e
mariam1223 <mariamgalal1111@gmail.com>
2023-12-28 16:08:17 +0200

    first commit

---
+++ j.js
@@ -0,0 +1,10 @@
+const menu =document.querySelector('.menu ');
+const menuIcon =document.querySelector('.menu i');
+const menuDet =document.querySelector('.c-left');
+function closemenu(){
+    menuDet.style.display = "none";
+}
+function openmenu (){
+    menuDet.style.display = "block";
+}
+
